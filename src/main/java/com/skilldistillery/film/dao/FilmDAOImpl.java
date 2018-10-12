2429a60fb1b3e581c2152cb7b0dcab154f1a1519
z-lamb <lamb.zachary@gmail.com>
2018-10-12 15:58:18 -0600

    better encapsulation of FilmDAOImpl

--- src/main/java/com/skilldistillery/film/dao/FilmDAOImpl.java
+++ src/main/java/com/skilldistillery/film/dao/FilmDAOImpl.java
@@ -31,38 +31,29 @@ public class FilmDAOImpl implements FilmDAO {
 	 */
 	@Override
 	public Film getFilmById(int filmId) throws SQLException {
-		
+
 		/*
-		 * Connection to the database and create new reference to a film object to be returned
+		 * Connection to the database and create new reference to a film object to be
+		 * returned
 		 */
 		Film film = null;
 		Connection conn = DriverManager.getConnection(URL, user, pass);
 
 		/*
-		 * SQL string to be passed to the prepared statement along with the requested ID which returns the query
-		 * If there is a query that is returned it will add each item requested to a new film object
-		 * After that has finished it will close the Prepared Statement and connection and return the film object
+		 * SQL string to be passed to the prepared statement along with the requested ID
+		 * which returns the query If there is a query that is returned it will add each
+		 * item requested to a new film object After that has finished it will close the
+		 * Prepared Statement and connection and return the film object
 		 */
 		String sql = "SELECT film.id, film.title, film.description, film.release_year, language.name, film.rental_duration, film.rental_rate, film.length, film.replacement_cost, film.rating, film.special_features, category.name FROM film JOIN language ON language.id = film.language_id JOIN film_category ON film.id = film_category.film_id JOIN category ON film_category.category_id = category.id WHERE film.id = ?";
-		
+
 		PreparedStatement stmt = conn.prepareStatement(sql);
 		stmt.setInt(1, filmId);
 		ResultSet filmResult = stmt.executeQuery();
 
 		if (filmResult.next()) {
 			film = new Film();
-			film.setId(filmResult.getInt("id"));
-			film.setTitle(filmResult.getString("title"));
-			film.setDescription(filmResult.getString("description"));
-			film.setReleaseYear(filmResult.getShort("release_year"));
-			film.setLanguageName(filmResult.getString("language.name"));
-			film.setRentalDuration(filmResult.getInt("rental_duration"));
-			film.setRentalRate(filmResult.getDouble("rental_rate"));
-			film.setLength(filmResult.getInt("length"));
-			film.setReplacementCost(filmResult.getDouble("replacement_cost"));
-			film.setRating(filmResult.getString("rating"));
-			film.setSpecialFeatures(filmResult.getString("Special_features"));
-			film.setCategory(filmResult.getString("category.name"));
+			film = setFilmObject(film, filmResult);
 			film.setActors(getActorsByFilmId(filmId));
 			film.setConditionList(getFilmConditionByFilmId(filmId));
 		}
@@ -148,7 +139,7 @@ public class FilmDAOImpl implements FilmDAO {
 	 */
 	@Override
 	public List<Film> getFilmsByActorId(int actorId) throws SQLException {
-		
+
 		/*
 		 * Connection to the database and create new object to be returned
 		 */
@@ -156,35 +147,23 @@ public class FilmDAOImpl implements FilmDAO {
 		Connection conn = DriverManager.getConnection(URL, user, pass);
 
 		/*
-		 * SQL string to be passed to the prepared statement along with the requested ID which returns the query
-		 * If there is a query that is returned it will add each item requested to a new film object
-		 * After that has finished it will close the Prepared Statement and connection and return the film list object
+		 * SQL string to be passed to the prepared statement along with the requested ID
+		 * which returns the query If there is a query that is returned it will add each
+		 * item requested to a new film object After that has finished it will close the
+		 * Prepared Statement and connection and return the film list object
 		 */
 		String sql = "SELECT film.id, title, description, release_year, language.name, rental_duration, rental_rate, length, replacement_cost, rating, special_features, category.name FROM film JOIN film_actor ON film.id = film_actor.film_id JOIN language ON language.id = film.language_id JOIN film_category ON film.id = film_category.film_id JOIN category ON film_category.category_id = category.id WHERE actor_id = ?";
-		
+
 		PreparedStatement stmt = conn.prepareStatement(sql);
 		stmt.setInt(1, actorId);
 		ResultSet rs = stmt.executeQuery();
 
 		while (rs.next()) {
 			Film film = new Film();
-
-			film.setId(rs.getInt("id"));
-			film.setTitle(rs.getString("title"));
-			film.setDescription(rs.getString("description"));
-			film.setReleaseYear(rs.getShort("release_year"));
-			film.setLanguageName(rs.getString("language.name"));
-			film.setRentalDuration(rs.getInt("rental_duration"));
-			film.setRentalRate(rs.getDouble("rental_rate"));
-			film.setLength(rs.getInt("length"));
-			film.setReplacementCost(rs.getDouble("replacement_cost"));
-			film.setRating(rs.getString("rating"));
-			film.setSpecialFeatures(rs.getString("special_features"));
-			film.setCategory(rs.getString("category.name"));
-
+			film = setFilmObject(film, rs);
 			films.add(film);
 		}
-		
+
 		stmt.close();
 		conn.close();
 		return films;
@@ -194,49 +173,48 @@ public class FilmDAOImpl implements FilmDAO {
 	 * Method to get Films By Keyword
 	 */
 	@Override
-	public List<Film> getFilmsByKeyword(String keyword) throws SQLException {
-		
+	public List<Film> getFilmsByKeyword(String keyword) {
+
 		/*
 		 * Connection to the database and create new object to be returned
 		 */
 		List<Film> films = new ArrayList<>();
-		Connection conn = DriverManager.getConnection(URL, user, pass);
+		Connection conn = null;
+		PreparedStatement stmt = null;
 
-		/*
-		 * SQL string to be passed to the prepared statement along with the requested ID which returns the query
-		 * If there is a query that is returned it will add each item requested to a new film object
-		 * After that has finished it will close the Prepared Statement and connection and return the film list object
-		 */
-		String sql = "SELECT film.id, film.title, film.description, film.release_year, language.name, film.rental_duration, film.rental_rate, film.length, film.replacement_cost, film.rating, film.special_features, category.name FROM film JOIN language ON language.id = film.language_id JOIN film_category ON film.id = film_category.film_id JOIN category ON film_category.category_id = category.id WHERE title LIKE ? OR description LIKE ?";
-		
-		PreparedStatement stmt = conn.prepareStatement(sql);
-		stmt.setString(1, "%" + keyword + "%");
-		stmt.setString(2, "%" + keyword + "%");
-		ResultSet rs = stmt.executeQuery();
+		try {
+			conn = DriverManager.getConnection(URL, user, pass);
 
-		while (rs.next()) {
-			Film film = new Film();
+			/*
+			 * SQL string to be passed to the prepared statement along with the requested ID
+			 * which returns the query If there is a query that is returned it will add each
+			 * item requested to a new film object After that has finished it will close the
+			 * Prepared Statement and connection and return the film list object
+			 */
+			String sql = "SELECT film.id, film.title, film.description, film.release_year, language.name, film.rental_duration, film.rental_rate, film.length, film.replacement_cost, film.rating, film.special_features, category.name FROM film JOIN language ON language.id = film.language_id JOIN film_category ON film.id = film_category.film_id JOIN category ON film_category.category_id = category.id WHERE title LIKE ? OR description LIKE ?";
 
-			film.setId(rs.getInt("id"));
-			film.setTitle(rs.getString("title"));
-			film.setDescription(rs.getString("description"));
-			film.setReleaseYear(rs.getShort("release_year"));
-			film.setLanguageName(rs.getString("language.name"));
-			film.setRentalDuration(rs.getInt("rental_duration"));
-			film.setRentalRate(rs.getDouble("rental_rate"));
-			film.setLength(rs.getInt("length"));
-			film.setReplacementCost(rs.getDouble("replacement_cost"));
-			film.setRating(rs.getString("rating"));
-			film.setSpecialFeatures(rs.getString("special_features"));
-			film.setCategory(rs.getString("category.name"));
-			film.setActors(getActorsByFilmId(rs.getInt("id")));
-			film.setConditionList(getFilmConditionByFilmId(rs.getInt("id")));
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, "%" + keyword + "%");
+			stmt.setString(2, "%" + keyword + "%");
+			ResultSet rs = stmt.executeQuery();
 
-			films.add(film);
+			while (rs.next()) {
+				Film film = new Film();
+				film = setFilmObject(film, rs);
+				film.setActors(getActorsByFilmId(rs.getInt("id")));
+				film.setConditionList(getFilmConditionByFilmId(rs.getInt("id")));
+				films.add(film);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-		
-		stmt.close();
-		conn.close();
 		return films;
 	}
 	
