b1ef91cd398324d79971bac0b4fa40107aae01cb
Henry Zelenka <hcz2@georgetown.edu>
2018-10-12 16:22:16 -0600

    Handle adding new films in FilmController

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.skilldistillery.film.dao.FilmDAOImpl;
 import com.skilldistillery.film.entities.Film;
@@ -31,6 +32,28 @@ public class FilmController {
 		}
 		mv.addObject("film", f);
 		mv.setViewName("WEB-INF/result.jsp");
+		return mv;
+	}
+
+	@RequestMapping(path = "NewFilm.do", method = RequestMethod.POST)
+	public ModelAndView newFilm(Film f) {
+		filmDAO.addFilm(f);
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("result");
+		return null;
+	}
+
+	@RequestMapping(path = "NewFilm.do", method = RequestMethod.POST)
+	public String newFilm(Film f, RedirectAttributes redir) {
+		filmDAO.addFilm(f);
+		redir.addFlashAttribute("film", f);
+		return "redirect:filmAdded.do";
+	}
+
+	@RequestMapping("filmAdded.do")
+	public ModelAndView filmAdded() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("result");
 		return mv;
 	}
 }
